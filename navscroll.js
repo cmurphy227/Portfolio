65858523b10c46c9d75b1815f0037a762e889c51
Connor <cmurphy227@west-mec.org>
2021-05-14 18:24:30 -0700

    Working on hamburger menu for mobile

---
+++ navscroll.js
@@ -0,0 +1,29 @@
+const fixNav = () => {
+const nav = document.querySelector(".nav");
+  if (window.scrollY > nav.offsetHeight + 500){ nav.classList.add("active"); }
+  else { nav.classList.remove("active"); }
+};
+
+window.addEventListener("scroll", fixNav);
+
+
+
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
--- test4.js
+++
@@ -1,7 +0,0 @@
-const fixNav = () => {
-const nav = document.querySelector(".nav");
-  if (window.scrollY > nav.offsetHeight + 500){ nav.classList.add("active"); }
-  else { nav.classList.remove("active"); }
-};
-
-window.addEventListener("scroll", fixNav);
