1d6bc858d3db3544101f5b8426e6a306bd650660
Connor <cmurphy227@west-mec.org>
2021-05-19 18:23:41 -0700

    Testing display buttons for projects portion

--- navscroll.js
+++ navscroll.js
@@ -1,3 +1,4 @@
+// Navscroll
 const fixNav = () => {
 const nav = document.querySelector(".nav");
   if (window.scrollY > nav.offsetHeight + 500){ nav.classList.add("active"); }
@@ -8,7 +9,7 @@ window.addEventListener("scroll", fixNav);
 
 
 
-
+// Hamburger menu
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 const navLink = document.querySelectorAll(".nav-link");
@@ -24,4 +25,32 @@ function mobileMenu() {
 function closeMenu() {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
+}
+
+
+// Back and Next buttons
+function displayChange() {
+let displaycount = 0;
+
+  document.getElementById("back").onclick = function backbtn(){
+  displaycount--;
+  if (displaycount == -1){
+    displaycount = 1;
+    }
+  }
+  document.getElementById("next").onclick = function nextbtn(){
+  displaycount++;
+  if (displaycount == 2){
+    displaycount = 0;
+    }
+  }
+
+  if (displaycount == 0){
+    document.getElementById("table2").style.display = "none";
+    document.getElementById("table").style.display = "table";
+  }
+  if (displaycount == 1){
+    document.getElementById("table2").style.display = "table";
+    document.getElementById("table").style.display = "none";
+  }
 }
